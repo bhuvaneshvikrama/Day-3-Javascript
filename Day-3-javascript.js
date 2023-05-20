1f77e834040739e7a2e6ff060949d9554d27b4cf
bhuvaneshvikrama <133791084+bhuvaneshvikrama@users.noreply.github.com>
2023-05-20 19:02:06 +0530

    Day 3 Assignment JavaScript

---
+++ Day-3-javascript.js
@@ -0,0 +1,6 @@
+// Step 1: Create the "fruits" array
+let fruits = ["apple", "banana", "orange", "grape", "mango"];
+
+// Step 2: Display the total number of fruits
+let totalFruits = fruits.length;
+console.log("Total number of fruits:", totalFruits);
